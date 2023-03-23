274bdd89d4cc4111bc4184626ec3b12a6ec24e71
smirrebinx <48405465+smirrebinx@users.noreply.github.com>
2023-03-23 13:00:53 +0100

    Fix styling for h2 and input form

--- code/src/components/ThoughtForm.js
+++ code/src/components/ThoughtForm.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ThoughtForm = ({ newThought, onNewThoughtChange, onFormSubmit }) => {
   return (
     <form className="thoughtForm" onSubmit={onFormSubmit}>
-      <h1>{'What\'s making you happy right now'}</h1>
+      <h2>{'What\'s making you happy right now?'}</h2>
       <textarea className="textArea" value={newThought} onChange={onNewThoughtChange} />
       <button className="formBtn" type="submit">❤️ Send Happy Thought! ❤️</button>
     </form>
