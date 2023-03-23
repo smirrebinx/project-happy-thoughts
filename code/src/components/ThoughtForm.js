d5ebca5bf8afc97d4d6d750a23eea6b7719b6ec3
smirrebinx <48405465+smirrebinx@users.noreply.github.com>
2023-03-23 17:41:50 +0100

    Add form label, aria hidden and sr-only

--- code/src/components/ThoughtForm.js
+++ code/src/components/ThoughtForm.js
@@ -1,13 +1,22 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 
 const ThoughtForm = ({ newThought, onNewThoughtChange, onFormSubmit }) => {
   return (
-    <form className="thought-form" onSubmit={onFormSubmit}>
-      <h2>{'What\'s making you happy right now?'}</h2>
-      <textarea placeholder="Type your happy thought here" className="text-area" value={newThought} onChange={onNewThoughtChange} />
+    <form className="thought-form" onSubmit={onFormSubmit} aria-label="Happy Thoughts Form">
+      <h2 aria-hidden="true">{'What\'s making you happy right now?'}</h2>
+      <label htmlFor="happy-thought-input" className="sr-only" aria-hidden="true">Type your happy thought here:</label>
+      <textarea
+        id="happy-thought-input"
+        placeholder="Type your happy thought here"
+        className="text-area"
+        value={newThought}
+        onChange={onNewThoughtChange}
+        aria-label="Happy thought input" />
       <button className="form-btn" type="submit">❤️ Send Happy Thought! ❤️</button>
     </form>
   );
 }
 
 export default ThoughtForm;
+
